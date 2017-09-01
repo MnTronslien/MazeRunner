819d4f104ab08388544fad6bdaeb39a73710ea71
Mntronslien <mntronslien@gmail.com>
2017-09-01 02:51:02 +0200

    Added 32x32 piece, looked into lighting again

--- Assets/MazeMenuScript.cs
+++ Assets/MazeMenuScript.cs
@@ -1,3 +1,23 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8b4a04cadec040a25d3225c02709a23beabc06fa9537415f0d246089eb6157d2
-size 840
+﻿using UnityEditor;
+using UnityEngine;
+public class MazeMenuScript : MonoBehaviour
+{
+    // Add a menu item named "CreateMazePiece" to Maze in the menu bar.
+    /// <summary>
+    /// Turns a slected object(s) into a Prefab and ads it to the list of set pieces
+    /// </summary>
+    [MenuItem("Maze/CreateMazePiece")]
+    static void DoCreateMazePiecePrefab()
+    {
+        Transform[] transforms = Selection.transforms;
+        foreach (Transform t in transforms)
+        {
+
+            MeshCollider mc = t.gameObject.AddComponent<MeshCollider>();
+            Object prefab = PrefabUtility.CreateEmptyPrefab("Assets/Resources/UnsortedMazePrefabs/" + t.gameObject.name + ".prefab");
+            PrefabUtility.ReplacePrefab(t.gameObject, prefab, ReplacePrefabOptions.ConnectToPrefab);
+            DestroyImmediate(t.gameObject);
+        }
+    }
+
+}
