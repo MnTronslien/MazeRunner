095c8ad6dd004c6cd2857eb46c166a140a1a8bb4
Mntronslien <mntronslien@gmail.com>
2017-09-03 02:49:42 +0200

    fixed assigning mesh to mesh colider in the generate prefab prossess

--- Assets/MazeMenuScript.cs
+++ Assets/MazeMenuScript.cs
@@ -12,11 +12,17 @@ public class MazeMenuScript : MonoBehaviour
         Transform[] transforms = Selection.transforms;
         foreach (Transform t in transforms)
         {
-
+            Debug.Log("Creating a prefab out of " + t.gameObject.name);
+            //Add mesh collider
             MeshCollider mc = t.gameObject.AddComponent<MeshCollider>();
+            mc.sharedMesh = t.gameObject.GetComponentInChildren<MeshFilter>().sharedMesh;
+            Debug.Log("Mesh collider with mesh added");
+
             Object prefab = PrefabUtility.CreateEmptyPrefab("Assets/Resources/UnsortedMazePrefabs/" + t.gameObject.name + ".prefab");
             PrefabUtility.ReplacePrefab(t.gameObject, prefab, ReplacePrefabOptions.ConnectToPrefab);
             DestroyImmediate(t.gameObject);
+
+            AssetDatabase.LoadAssetAtPath<GameObject>(AssetDatabase.GetAssetPath(Selection.activeObject));
         }
     }
 
